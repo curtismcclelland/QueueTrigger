a8dd8c7f840bcf0b4af70a9f70cd663cb3547a8c
Curtis McClelland <curtis.mcclelland@avanade.com>
2022-10-31 16:54:27 -0400

    Init

---
+++ QueueTrigger2.cs
@@ -0,0 +1,19 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public class QueueTrigger2
+    {
+        [FunctionName("QueueTrigger2")]
+        public void Run([QueueTrigger("myqueue-items", Connection = "southcentralqueue_STORAGE")]string myQueueItem, 
+        [Blob("outcontainer/{rand-guid}", FileAccess.Write, Connection="southcentralqueue_STORAGE")] out string myBlob1, ILogger log)
+        {
+            myBlob1 = myQueueItem;
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
